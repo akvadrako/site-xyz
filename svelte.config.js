6553e74249cfc0a33c5ccb5fa3e8232d65ade60f
Devin Bayer <dev@doubly.so>
2021-11-06 17:43:49 +0100

    try static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,19 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
-import adapter from '@sveltejs/adapter-netlify'
+//import adapter from '@sveltejs/adapter-netlify'
+import adapter from '@sveltejs/adapter-static'
 
 const config = {
 	kit: {
-		adapter: adapter(), // currently the adapter does not take any options
-		target: '#svelte',
-		prerender: {
-			crawl: true,
-			enabled: true,
-			onError: 'continue',
-			entries: ['*'],
-		},
+            adapter: adapter({}),
+            // currently the adapter does not take any options
+            target: '#svelte',
+            prerender: {
+                crawl: true,
+                enabled: true,
+                onError: 'continue',
+                entries: ['*'],
+            },
 	},
 	extensions: [".svelte", ...mdsvexConfig.extensions],
 	preprocess: [mdsvex(mdsvexConfig)]
