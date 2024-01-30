616df2f3b2935c5c61c2103bfb6bb8c7367ee457
Devin Bayer <dev@doubly.so>
2024-01-30 21:33:06 +0100

    changes

--- src/comp/Hero.svelte
+++ src/comp/Hero.svelte
@@ -2,57 +2,41 @@
 import { Photo } from '$comp';
 </script>
 
-<div id="backdrop">
-    <div id="wrap">
-        <Photo src="/media/image_top.jpg" sizes="100vw" />
-        <div id="mask">
-            <div />
-            <img alt="mask" src="/media/hero-mask.svg" height="100%" width="100%" />
-        </div>
+<div class="hero">
+    <div class="inner">
+        <img class="mask" alt="mask" src="/media/hero-mask.svg" />
+        <Photo src="/media/image_top.jpg" />
     </div>
 </div>
 
 <style type="postcss">
-#mask {
+.hero {
+    --ratio: calc(3707 / 1980); 
+}
+
+.mask {
     position: fixed;
     z-index: 20;
     top: 0;
-    width: 100%;
-}
-
-#mask img {
-    display: block;
-    height: max(50vh, min(calc(100vw / 3707 * 1980), calc(100vh - var(--nav-height))));
-    object-fit: fill;
-    object-position: 50% 50%;
-}
-
-#mask div {
-    height: var(--nav-height);
-    background: inherit;
-}
-
-#backdrop {
     margin-top: var(--nav-height);
-    width: 100%;
-    height: max(50vh, min(calc(100vw / 3707 * 1980), calc(100vh - var(--nav-height))));
-    overflow: clip;
-    clip-path: inset(0 0 0 0);
-    position: relative;
+    box-shadow: 0 0 0 400px var(--theme-bg);
 }
 
-#wrap {
-    position: absolute;
-    bottom: 0;
-    height: 100%;
-    display: flex;
+.inner {
+    margin: auto;
+}
 
-    :global(.photo) {
-        height: 100%;
-        width: 100%;
-        object-fit: cover;
-        object-position: 40% bottom;
-    }
+.inner, .mask {
+    /* fill screen with hero image without cropping */
+    height: min(calc((100vw - 4rem) / var(--ratio)), calc(100vh - var(--nav-height)));
+    aspect-ratio: var(--ratio);
+}
+
+.hero {
+    overflow: clip;
+    margin-top: var(--nav-height);
+    clip-path: margin-box;
+    position: relative;
 }
 
 </style>
